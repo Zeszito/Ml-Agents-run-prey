635ecf203041f4e2e81ab889b8f4867d71811fd9
Moises <moises.moreira.bio@gmail.com>
2022-07-11 09:29:54 +0100

    add mais obestaculos

---
+++ AI_Moises/Assets/Scripts/Predador_Agent.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.MLAgents;
+using UnityEngine;
+
+public class Predador_Agent : Agent
+{
+    // Start is called before the first frame update
+  
+    
+}
