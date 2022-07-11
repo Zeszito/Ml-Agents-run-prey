90dddf4b1df9377d7ea95bc54b5d4fcf40961e46
Moises <moises.moreira.bio@gmail.com>
2022-07-11 11:31:30 +0100

    Fake presa

--- AI_Moises/Assets/Scripts/FakePresa.cs
+++ AI_Moises/Assets/Scripts/FakePresa.cs
@@ -5,14 +5,62 @@ using UnityEngine;
 public class FakePresa : MonoBehaviour
 {
     // Start is called before the first frame update
+    Vector3 dir;
+    Rigidbody rg;
+    public int mul = 20;
     void Start()
     {
-        
+        switch (Random.Range(0, 4))
+        {
+            case 0:
+                dir = Vector3.forward;
+                break;
+            case 1:
+                dir = -Vector3.forward;
+                break;
+            case 2:
+                dir = Vector3.right;
+                break;
+            case 3:
+                dir = Vector3.left;
+                break;
+        }
+       
+       rg = this.GetComponent<Rigidbody>();
+       this.transform.localPosition = new Vector3(Random.Range(-10, 10), this.transform.localPosition.y, Random.Range(-10, 10));
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        rg.AddForce(dir*mul);
+        if((((int)Time.fixedTime) % 5) == 0)
+        {
+            dir = -dir;
+        }
+ 
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.tag =="Obestaculo" || collision.gameObject.tag == "wall")
+        {
+            switch (Random.Range(0, 4))
+            {
+                case 0:
+                    dir = Vector3.forward;
+                    break;
+                case 1:
+                    dir = -Vector3.forward;
+                    break;
+                case 2:
+                    dir = Vector3.right;
+                    break;
+                case 3:
+                    dir = Vector3.left;
+                    break;
+            }
+        }
+  
     }
 }
